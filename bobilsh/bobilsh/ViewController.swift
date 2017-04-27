46607496777fd37d64e5e38ccde456edacb39034
Ethan <larockin02@gmail.com>
2017-04-27 13:19:30 -0700

    Uplaod 2
    got font slider to work and made color buttons

--- bobilsh/bobilsh/ViewController.swift
+++ bobilsh/bobilsh/ViewController.swift
@@ -9,27 +9,44 @@
 import UIKit
 
 class ViewController: UIViewController {
+ 
 
-    @IBOutlet weak var labelthing: UILabel!
-    @IBOutlet weak var LABEL: UILabel!
-    /*
-    @IBAction func font(_ sender: UISlider)
-    {
-        labelthing.text = String(sender.value)
-    }*/
+    @IBOutlet weak var Inputlabel: UILabel!
+    @IBOutlet weak var bob: UILabel!
+    @IBAction func font(_ sender: UISlider){
+        let senderValue = CGFloat(sender.value)
+        bob?.font = UIFont(name: (bob?.font.fontName)!, size:senderValue)
+    }
     
     @IBOutlet weak var text: UITextField!
     
     @IBAction func button1(_ sender: Any) {
-        LABEL.text = text.text
+       bob.text = text.text
+        Inputlabel.text = text.text
     }
-    override func viewDidLoad()
-    {
-        super.viewDidLoad()
+    @IBAction func Green(_ sender: Any) {
+        bob.textColor = UIColor(red: 0, green: 255, blue: 0, alpha: 1)
+        Inputlabel.textColor = UIColor(red: 0, green: 255, blue: 0, alpha: 1)
     }
 
-    override func didReceiveMemoryWarning()
-    {
+    @IBAction func Blue(_ sender: Any) {
+        bob.textColor = UIColor(red: 0, green: 0, blue: 255, alpha: 1)
+        Inputlabel.textColor = UIColor(red: 0, green: 0, blue: 255, alpha: 1)
+    }
+    @IBAction func Red(_ sender: Any) {
+        bob.textColor = UIColor(red: 255, green: 0, blue: 0, alpha: 1)
+         Inputlabel.textColor = UIColor(red: 255, green: 0, blue: 0, alpha: 1)
+    }
+    @IBAction func Purple(_ sender: Any) {
+        bob.textColor = UIColor(red: 120, green: 0, blue: 255, alpha: 1)
+         Inputlabel.textColor = UIColor(red: 120, green: 0, blue: 255, alpha: 1)
+    }
+    @IBAction func Orange(_ sender: Any) {
+       bob.textColor = UIColor(red: 255, green: 255, blue: 0, alpha: 1)
+        Inputlabel.textColor = UIColor(red: 255, green: 255, blue: 0, alpha: 1)
+    }
+
+    override func didReceiveMemoryWarning(){
         super.didReceiveMemoryWarning()
     }
     
