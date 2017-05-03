1f3ac6050fa7843b76863335a2ec31702dace9fd
Ethan <larockin02@gmail.com>
2017-05-03 13:16:34 -0700

    Text game 2.0
    added buttons

--- TEXT-GAME-2.0/TEXT-GAME-2.0/ViewController.swift
+++ TEXT-GAME-2.0/TEXT-GAME-2.0/ViewController.swift
@@ -13,13 +13,21 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    @IBOutlet weak var Name: UILabel!
+    @IBOutlet weak var Label: UILabel!
+    @IBAction func Button1(_ sender: Any) {
+        //Button1.text = Button1.("classTopTextField")
+    }
+    @IBAction func Button(_ sender: Any) {
+    }
 
-
+    
 }
 
