fe1029cd6ca0354efa52bf147512ed7c1c8d75ca
Ethan <larockin02@gmail.com>
2017-05-04 13:15:38 -0700

    Text based game 2.0
    added name func. and buttons now working on story.

--- TEXT-GAME-2.0/TEXT-GAME-2.0/ViewController.swift
+++ TEXT-GAME-2.0/TEXT-GAME-2.0/ViewController.swift
@@ -13,21 +13,32 @@ class ViewController: UIViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
-       
     }
 
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-    @IBOutlet weak var Name: UILabel!
+    
+    @IBOutlet weak var Next: UIButton!
+    @IBOutlet weak var Name: UITextField!
+    var username = "";
     @IBOutlet weak var Label: UILabel!
-    @IBAction func Button1(_ sender: Any) {
-        //Button1.text = Button1.("classTopTextField")
-    }
+    @IBOutlet weak var Button1: UIButton!
+    @IBOutlet weak var Button: UIButton!
     @IBAction func Button(_ sender: Any) {
     }
+    @IBAction func Button1(_ sender: UIButton) {
+        Label.text = "What is Your name"
+        Name.isHidden = false
+        Next.isHidden = false
+        Button1.isHidden = true
+        Button1.setTitle("Yes", for: .normal)
+    }
 
-    
+    @IBAction func Next(_ sender: Any) {
+        username = Name.text!;
+        Label.text = ("Hello",username)
+    }
 }
 
