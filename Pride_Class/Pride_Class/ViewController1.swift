970fef16e13c73065a69cf7489d9fbe8473e808d
Ethan <larockin02@gmail.com>
2017-06-02 12:05:05 -0700

    working on schuale

---
+++ Pride_Class/Pride_Class/ViewController1.swift
@@ -0,0 +1,21 @@
+import UIKit
+
+class ViewController1: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    @IBOutlet weak var block1: UILabel!
+    @IBOutlet weak var block2: UILabel!
+    @IBOutlet weak var block3: UILabel!
+    @IBOutlet weak var block4: UILabel!
+    
+    
+}
+
---
+++ Pride_Class/Pride_Class/ViewController2.swift
@@ -0,0 +1,51 @@
+
+import UIKit
+
+class ViewController2: UIViewController {
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+    @IBAction func AlecJones(_ sender: Any) {
+        block1.text = "Alec"
+    }
+    @IBAction func AndreDove(_ sender: Any) {
+    }
+    @IBAction func AnneDeitch(_ sender: Any) {
+    }
+    @IBAction func BrynnHand(_ sender: Any) {
+    }
+    @IBAction func CassieKaleohanaHauanio(_ sender: Any) {
+    }
+    @IBAction func CharlieCoffin(_ sender: Any) {
+    }
+    @IBAction func HoustonRobison(_ sender: Any) {
+    }
+    @IBAction func JaneTucker(_ sender: Any) {
+    }
+    @IBAction func JenniferBoyd(_ sender: Any) {
+    }
+    @IBAction func LaceyBolin(_ sender: Any) {
+    }
+    @IBAction func MaggieSutrov(_ sender: Any) {
+    }
+    @IBAction func MandieSoto(_ sender: Any) {
+    }
+    @IBAction func MattMiller(_ sender: Any) {
+    }
+    @IBAction func SeanHopf(_ sender: Any) {
+    }
+    @IBAction func TomLee(_ sender: Any) {
+    }
+    @IBAction func TommyGlasser(_ sender: Any) {
+    }
+    @IBAction func TriciaNovak(_ sender: Any) {
+    }
+}
